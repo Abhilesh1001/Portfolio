8c3962be9f8c461dc2731cbf9fe572b1ec4b7335
Abhilesh1001 <104245983+Abhilesh1001@users.noreply.github.com>
2024-02-16 02:09:35 +0530

    contactformadd

--- src/action/messageaction.tsx
+++ src/action/messageaction.tsx
@@ -1,9 +1,21 @@
 'use server'
+import axios from "axios"
+import { baseurl } from "@/data/dataall"
 
 
-export const habdleSubmit=(formData:FormData)=>{
-    console.log(formData)
-
-    return 'success fully updated data'
+export const habdleSubmit = async (formData: FormData) => {
+    const data:any = {
+        "name": formData.get('name'),
+        "email": formData.get('email'),
+        "message":formData.get('message')
+    }
+    
+    try {
+        const res: any = await axios.post(`${baseurl}/pro/message/`, data);
+        return res?.data?.msg;
+    } catch (error) {
+        console.error("Error occurred:", error);
+        return 'error occurred while updating data';
+    }
 
 }
--- src/data/dataall.tsx
+++ src/data/dataall.tsx
@@ -1,4 +1,6 @@
 
 
 // https://abhileshport.pythonanywhere.com/
+//http://127.0.0.1:8000/
+
 export const baseurl = 'https://abhileshport.pythonanywhere.com'
