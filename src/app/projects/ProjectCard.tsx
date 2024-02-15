a52009b95cb3064f1c3a438f23bc085d0402c322
Abhilesh1001 <104245983+Abhilesh1001@users.noreply.github.com>
2024-02-15 19:06:02 +0530

    backenadd

--- src/app/projects/ProjectCard.tsx
+++ src/app/projects/ProjectCard.tsx
@@ -2,18 +2,19 @@ import Imageurl from '../assets/3.png'
 import Imageurl4 from '../assets/4.png'
 
 import Image from 'next/image'
+import { baseurl } from '@/data/dataall'
 
 
 interface projectprops{
 
     project:{
         name: string,
-        description: string,
-        imageUrl: string,
-        skilUsed : string,
+        desc: string,
+        image: string,
+        skil : string,
         githubLinkFrontend: string,
         githubLinkBAckend: string,
-        backendLink: string,
+        backendLink:string,
         frontendLink: string,
     }
      
@@ -22,14 +23,16 @@ interface projectprops{
 
 const ProjectCard = (props:projectprops) => {
     const {project} = props
-    console.log(project.imageUrl)
+    const image = `${baseurl}${project.image}`
+    console.log(image)
 
     return (
       <div className="max-w-md rounded overflow-hidden shadow-lg  bg-green-800 dark:bg-gray-700">
-        <Image className="w-full h-64 object-cover" src={Imageurl4} alt={project.name} width={800} height={800} />
+        <img className="p-2  rounded object-cover " src={image} alt={project.name}  />
           <div className="font-bold text-xl mb-2 mx-2 mt-2 text-gray-50">{project.name}</div>
+          <div className="font-bold  mb-2 mx-2 mt-2 text-gray-50">Skills : {project.skil}</div>
         <div className="px-6 py-2">
-          <p className="text-gray-50 text-base">{project.description}</p>
+          <p className="text-gray-50 text-base">{project.desc}</p>
         </div>
         <div className="px-6 py-4">
           <a href={project.githubLinkFrontend} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-gray-300">GitHub Frontend</a>
